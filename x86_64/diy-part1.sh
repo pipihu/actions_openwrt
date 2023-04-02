414ceca97080040b1778c1fa200e4797a80a6ea9
pipihu <pipihu@gmail.com>
2023-04-02 23:22:40 +0800

    update: x86 add passwall,kernel version 6.1.x

--- x86_64/diy-part1.sh
+++ x86_64/diy-part1.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 TOKEN=$1
 
-# Add a feed source
+# Add helloworld
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# Add passwall
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
 
 # Add SSTP Client
 git clone https://$TOKEN@github.com/pipihu/openwrt-sstp.git package/openwrt-sstp
