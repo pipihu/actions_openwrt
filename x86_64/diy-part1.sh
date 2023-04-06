cdb286a53b097747f936220796715770ed6fa821
pipihu <pipihu@gmail.com>
2023-04-06 21:08:28 +0800

    update: x86 remove passwall

--- x86_64/diy-part1.sh
+++ x86_64/diy-part1.sh
@@ -4,9 +4,5 @@ TOKEN=$1
 # Add helloworld
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-# Add passwall
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
-echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
-
 # Add SSTP Client
 git clone https://$TOKEN@github.com/pipihu/openwrt-sstp.git package/openwrt-sstp
