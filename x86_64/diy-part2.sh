0d091ee549db1817f404f976a26a3e4a9812cc2a
pipihu <pipihu@gmail.com>
2023-04-13 14:29:49 +0800

    fix: ssrp tab switching error

--- x86_64/diy-part2.sh
+++ x86_64/diy-part2.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# disalbe vmlcsd port
 sed -i 's/	commit firewall/	set firewall.@rule[-1].enabled="0"\n&/' feeds/luci/applications/luci-app-vlmcsd/root/etc/uci-defaults/luci-vlmcsd
+
+# fix ssrp bug
+if ! grep -q "set uhttpd.main.max_requests=50" feeds/helloworld/luci-app-ssr-plus/root/etc/uci-defaults/luci-ssr-plus; then
+    sed -i '/commit firewall/a\	set uhttpd.main.max_requests=50\n	commit uhttpd' feeds/helloworld/luci-app-ssr-plus/root/etc/uci-defaults/luci-ssr-plus
+fi
