b50de8a91509ba0ebf9f9edb6ff14dce4b474d12
pipihu <pipihu@gmail.com>
2024-03-16 18:01:28 +0800

    Revert "Update x86_64-mini.config"
    This reverts commit 865b3135ffa3cc700ab91098513de9d81eda757d.

--- x86_64/diy-part1.sh
+++ x86_64/diy-part1.sh
@@ -2,7 +2,7 @@
 TOKEN=$1
 
 # Add helloworld
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # Add Openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git package/openclash
