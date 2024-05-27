0a784506a9176727f68dc0babd5acb56c54ec6b9
pipihu <pipihu@gmail.com>
2024-05-28 07:02:46 +0800

    Update diy-part2.sh

--- x86_64/diy-part2.sh
+++ x86_64/diy-part2.sh
@@ -3,10 +3,6 @@
 # disalbe vmlcsd port
 sed -i 's/	commit firewall/	set firewall.@rule[-1].enabled="0"\n&/' feeds/luci/applications/luci-app-vlmcsd/root/etc/uci-defaults/luci-vlmcsd
 
-# modify shadowsocks-rust pkg_hash
-sed -i 's/1c009fc412588cae6145d37382fca35708e3b79f806a90d5cb0fa24e0097d09f/080d2042eede744d2182ebb47929b504cd42ef8e0eefacdc5ece402f99328ea8/' feeds/helloworld/shadowsocks-rust/Makefile
-
-
 # fix ssrp bug
 # if ! grep -q "set uhttpd.main.max_requests=50" feeds/helloworld/luci-app-ssr-plus/root/etc/uci-defaults/luci-ssr-plus; then
 #     sed -i '/commit firewall/a\	set uhttpd.main.max_requests=50\n	commit uhttpd' feeds/helloworld/luci-app-ssr-plus/root/etc/uci-defaults/luci-ssr-plus
