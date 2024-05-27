b5d25b71cf1efb7cb04f3c235d1ea9666d34a61a
pipihu <pipihu@gmail.com>
2024-05-27 20:53:11 +0800

    ramips： 删除

--- ramips/XiaoMi-R3G-diy-part1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TOKEN=$1
-
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-# Add SSTP Client
-git clone https://$TOKEN@github.com/pipihu/openwrt-sstp.git package/openwrt-sstp
