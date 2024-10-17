1fba190cad0585f0decd30a5a37bbaf84318e8a8
pipihu <pipihu@gmail.com>
2024-10-18 07:11:12 +0800

    Update diy-part2.sh
    停用禁用vmlcsd端口映射的脚本

--- x86_64/diy-part2.sh
+++ x86_64/diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # disalbe vmlcsd port
-sed -i 's/	commit firewall/	set firewall.@rule[-1].enabled="0"\n&/' feeds/luci/applications/luci-app-vlmcsd/root/etc/uci-defaults/luci-vlmcsd
+# sed -i 's/	commit firewall/	set firewall.@rule[-1].enabled="0"\n&/' feeds/luci/applications/luci-app-vlmcsd/root/etc/uci-defaults/luci-vlmcsd
 
 # fix ssrp bug
 # if ! grep -q "set uhttpd.main.max_requests=50" feeds/helloworld/luci-app-ssr-plus/root/etc/uci-defaults/luci-ssr-plus; then
