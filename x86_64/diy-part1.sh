cf1853dcac3e2e8147226ce3ba39e5e5dc96e24e
pipihu <pipihu@gmail.com>
2024-10-28 10:28:54 +0800

    Update diy-part1.sh

--- x86_64/diy-part1.sh
+++ x86_64/diy-part1.sh
@@ -5,7 +5,7 @@ TOKEN=$1
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 
 # Add Openclash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package/openclash
+# git clone --depth=1 https://github.com/vernesong/OpenClash.git package/openclash
 
 # Add SSTP Client
 git clone https://$TOKEN@github.com/pipihu/openwrt-sstp.git package/openwrt-sstp
