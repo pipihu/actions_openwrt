e61834acc696731c7018681c867c29f6a56cd050
pipihu <pipihu@qq.com>
2025-08-25 11:21:26 +0800

    diy-part1.sh: 默认已启用helloworld，注释相关脚本

--- x86_64/diy-part1.sh
+++ x86_64/diy-part1.sh
@@ -2,7 +2,7 @@
 TOKEN=$1
 
 # Add helloworld
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 
 # Add Openclash
 # git clone --depth=1 https://github.com/vernesong/OpenClash.git package/openclash
